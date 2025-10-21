ee3d3795b8ffaace62c702232ab1c2801fffab9f
carolinileticia <leticiacsoares18@gmail.com>
2025-10-20 21:42:12 -0300

    Add files via upload
    rascunho da aula do dia 20/10

---
+++ aula 20.10/script.js
@@ -0,0 +1,6 @@
+function exibir(){
+    document.getElementById('texto').innerHTML = ' seu mouse está sobre o texto';
+}
+function ocultar(){
+    document.getElementById('texto').innerHTML = '';
+}
